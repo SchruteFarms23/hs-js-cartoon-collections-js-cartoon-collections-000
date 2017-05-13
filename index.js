27b69d5c55ae0edc8a35ef64ccec3250a935c943
Roman Mulladzhanov <romanmullady@gmail.com>
2017-05-13 17:46:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+  var arrTwo=[];
+  for(var i=0;i<dwarves.length;i++){
+    arrTwo.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return `${arrTwo.join("")}`
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arrTwo=[];
+  for(var i=0;i<planeteerCalls.length;i++){
+    arrTwo.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return arrTwo
 }
 
 function longPlaneteerCalls(words) {
+  for(var i=0;i<words.length;i++){
+    if(words[i].length>4){
+      return true
+    }
+    else{
+      return false
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  var cheese=["cheddar","gouda","camembert"];
+  for(var i=0;i<cheese.length;i++){
+    if(foods.includes(cheese[i])===true){
+      return cheese[i]
+    }
+    else{
+      return "no cheese!"
+    }
+  }
 }
